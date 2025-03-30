02719b57c7a917b8b0f09eec2b1331ce831302ba
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-03-30 19:26:12 +0700

    feat: updating icon for windows explorer

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <QIcon>
 
 int main(int argc, char *argv[]) {
-  QIcon appIcon;
   QApplication app(argc, argv);
-  MainWindow w;
+  QIcon appIcon;
   appIcon.addFile(":/icons/excelConvert32.ico", QSize(32, 32));
   appIcon.addFile(":/icons/excelConvert64.ico", QSize(64, 64));
   appIcon.addFile(":/icons/excelConvert128.ico", QSize(128, 128));
   app.setWindowIcon(appIcon);
+  MainWindow w;
   w.show();
   return app.exec();
 }
