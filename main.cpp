eaa03e0ca65bd941f84b05f3dcbe5e7ef6d61a8b
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-03-30 19:20:28 +0700

    style: updating icons

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char *argv[]) {
   QIcon appIcon;
   QApplication app(argc, argv);
   MainWindow w;
-  appIcon.addFile(":/icons/excelConvert32.png", QSize(32, 32));
-  appIcon.addFile(":/icons/excelConvert64.png", QSize(64, 64));
-  appIcon.addFile(":/icons/excelConvert128.png", QSize(128, 128));
+  appIcon.addFile(":/icons/excelConvert32.ico", QSize(32, 32));
+  appIcon.addFile(":/icons/excelConvert64.ico", QSize(64, 64));
+  appIcon.addFile(":/icons/excelConvert128.ico", QSize(128, 128));
   app.setWindowIcon(appIcon);
   w.show();
   return app.exec();
