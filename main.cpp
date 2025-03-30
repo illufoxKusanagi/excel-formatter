9f698b6f7942371f3adfee26877f7d75ececca2c
Arief Satria <128447209+illufoxKusanagi@users.noreply.github.com>
2025-03-30 19:15:45 +0700

    style: set app icon

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "pages/mainwindow.h"
 
 #include <QApplication>
+#include <QIcon>
 
 int main(int argc, char *argv[]) {
+  QIcon appIcon;
   QApplication a(argc, argv);
   MainWindow w;
+  appIcon.addFile(":/icons/excelConvert32.png", QSize(32, 32));
+  appIcon.addFile(":/icons/excelConvert64.png", QSize(64, 64));
+  appIcon.addFile(":/icons/excelConvert128.png", QSize(128, 128));
   w.show();
   return a.exec();
 }
